4b40518e88226df83eec4282446e1913426ddb76
Groverok <grover666@gmail.com>
2016-11-24 17:31:08 +0300

    Revert "Revert "new tests K*rwa mac!""
    This reverts commit cfe714d74fd9ac655f8343dd34dabdaf60adac40.

---
+++ test/Pages/SlowExternalPage.cs
@@ -0,0 +1,18 @@
+﻿using Epam.JDI.Core.Interfaces.Common;
+using JDI_Web.Selenium.Elements.Common;
+using JDI_Web.Selenium.Elements.Composite;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Pages
+{
+    public class SlowExternalPage : WebPage
+    {
+        [FindsBy(How = How.CssSelector, Using = ".message__title")]
+        public Text textWillAppearIn30Sseconds ;                           
+    }
+}
---
+++ test/Pages/SlowResourcesPage.cs
@@ -0,0 +1,19 @@
+﻿using JDI_Web.Attributes;
+using JDI_Web.Selenium.Elements.Common;
+using JDI_Web.Selenium.Elements.Complex.Table;
+using JDI_Web.Selenium.Elements.Composite;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Pages
+{
+    public class SlowResourcesPage : WebPage
+    {
+        [FindsBy(How = How.CssSelector, Using = ".example>h3")]
+        public Text SlowResourcesText;                          
+    }
+}
