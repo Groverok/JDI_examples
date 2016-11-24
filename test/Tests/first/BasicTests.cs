f9348af38d517c7f9654f9fc69b69155c660081b
Groverok <grover666@gmail.com>
2016-11-24 17:27:45 +0300

    new tests K*rwa mac!

--- test/Tests/first/BasicTests.cs
+++ test/Tests/first/BasicTests.cs
@@ -10,47 +10,81 @@ namespace test.Tests.firts
 {
     public class Class1
     {
-        private ICheckBox _checkBoxWater = TestSite.CheckBoxPage.CheckBox; //1 test
+        private ICheckBox _checkBoxWater = TestSite.CheckBoxPage.CheckBox; 
 
-        private IText _textItem = TestSite.ABPage.Text;  //2 test
+        private IText _textItem = TestSite.ABPage.Text;  
         private string textOnOurPage = "A/B Test Control";
 
-        private IDropDown<Select> options;  //3 test
+        private IDropDown<Select> options;  
         private IDropDown<Select> selectDrop => options;
 
-        private LoginForm OurForm = TestSite.LoginPage.LoginForm;  //4 test
+        private LoginForm OurForm = TestSite.LoginPage.LoginForm;  
         private IButton OurButton = TestSite.MainAuthPage.LogOutButton;
 
+        private IText _textItemSlow = TestSite.SlowResourcesPage.SlowResourcesText; 
+        private string textOnSlowPage = "Slow Resources";
+
+        private IText textWhichShouldBeAppear = TestSite.SlowExternalPage.textWillAppearIn30Sseconds; 
+        private string textOnSlowExternalPage = " Application error ";
+
+        private IButton GalleryTab = TestSite.DisappearingElementsPage.OurGalleryTab;
+
+
+        //[Test]
+        //public void TestCheckboxes()
+        //{
+        //    TestSite.CheckBoxPage.Open();
+        //    TestSite.CheckBoxPage.CheckBox.Uncheck();
+        //    Assert.IsFalse(TestSite.CheckBoxPage.CheckBox.IsChecked());
+        //}
+
+        //[Test]
+        //public void GetTextOnOurPageTest()
+        //{
+        //    TestSite.ABPage.Open();
+        //    Assert.AreEqual(_textItem.GetText, textOnOurPage);
+        //}
+
+        //[Test]
+        //public void SelectDropDownTest()
+        //{
+        //    TestSite.DropDownPage.Open();
+        //    TestSite.DropDownPage.options.Select("Option 1");
+
+        //}
+
+        //[Test]
+        //public void FillFormTest()
+        //{
+        //    TestSite.LoginPage.Open();
+        //    OurForm.Submit(User.DEFAULT_CONTACT);
+        //    Assert.IsTrue(OurButton.Displayed);
+        //}
+
+        //[Test]
+        //public void WaitForRequestTest()
+        //{
+        //    TestSite.SlowResourcesPage.Open();
+        //    _textItemSlow.WaitText(textOnSlowPage);
+        //    Assert.AreEqual(_textItemSlow.GetText, textOnSlowPage);
+        //}
+
+        //[Test]
+        //public void WaitForAppearingTextTest()
+        //{
+        //    TestSite.SlowExternalPage.Open();
+        //    textWhichShouldBeAppear.WaitText(textOnSlowExternalPage);
+        //}
 
         [Test]
-        public void TestCheckboxes()
+        public void DisappearingElementsTest()
         {
-            TestSite.CheckBoxPage.Open();
-            TestSite.CheckBoxPage.CheckBox.Uncheck();
-            Assert.IsFalse(TestSite.CheckBoxPage.CheckBox.IsChecked());
-        }
+            TestSite.DisappearingElementsPage.Open();
+            Assert.IsFalse(TestSite.DisappearingElementsPage.OurGalleryTab.Displayed);
+            TestSite.DisappearingElementsPage.Refresh();
+            Assert.IsTrue(TestSite.DisappearingElementsPage.OurGalleryTab.Displayed);
 
-        [Test]
-        public void GetTextOnOurPageTest()
-        {
-            TestSite.ABPage.Open();
-            Assert.AreEqual(_textItem.GetText, textOnOurPage);
-        }
 
-        [Test]
-        public void SelectDropDownTest()
-        {
-            TestSite.DropDownPage.Open();
-            TestSite.DropDownPage.options.Select("Option 1");
         }
-
-        [Test]
-        public void FillFormTest()
-        {
-            TestSite.LoginPage.Open();
-            OurForm.Submit(User.DEFAULT_CONTACT);
-            Assert.IsTrue(OurButton.Displayed);
-        }
-
     }
 }
